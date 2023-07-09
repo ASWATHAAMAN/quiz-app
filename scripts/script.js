fb6bcb052e92a497cbde130cdd36dff0d7a2235e
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-07-09 22:48:15 +0530

    js starts

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 `use strict`;
+
+// select elements
+const roundEl = document.querySelector(`.round`);
+// function
+
+// eventListener
+roundEl.addEventListener(`click`, () => {
+  roundEl.classlist.remove(`round`);
+  roundEl.classlist.add(`small-round`);
+});
